a494e5946698fa57e94ef4f3bbced5399ccbeb8e
shawnwildermuth <shawn@wildermuth.com>
2024-04-10 20:33:22 -0400

    Product API Works

---
+++ src/Foots/Foots.API/Apis/ProductApi.cs
@@ -0,0 +1,49 @@
+﻿using Foots.API.Data;
+using Microsoft.EntityFrameworkCore;
+using WilderMinds.MinimalApiDiscovery;
+using static Microsoft.AspNetCore.Http.Results;
+
+namespace Foots.API.Apis;
+
+public class ProductApi : IApi
+{
+  public void Register(IEndpointRouteBuilder app)
+  {
+    var group = app.MapGroup("/api/products");
+
+    group.MapGet("", GetProducts);
+    group.MapGet("{id:int}", GetProduct);
+  }
+
+  private async Task<IResult> GetProduct(FootsContext ctx, int id)
+  {
+    var product = await ctx.Products
+      .Include(p => p.Category)
+      .Where(p => p.Id == id)
+      .FirstOrDefaultAsync();
+
+    if (product is null) return NotFound();
+    return Ok(product);
+  }
+
+  private async Task<IResult> GetProducts(FootsContext ctx, string style = "")
+  {
+    if (style == "brief")
+    {
+      return Ok(await ctx.Products
+        .Include(p => p.Category)
+        .Select(p => new
+        {
+          p.Id,
+          p.Description,
+          p.ImageUrl,
+          Category = p.Category != null ? p.Category.Name : ""
+        })
+        .ToListAsync());
+    }
+
+    return Ok(await ctx.Products
+      .Include(p => p.Category)
+      .ToListAsync());
+  }
+}
--- src/Foots/Foots.API/Program.cs
+++ src/Foots/Foots.API/Program.cs
@@ -1,6 +1,8 @@
+using Foots.API.Apis;
 using Foots.API.Data;
 using Foots.API.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using WilderMinds.MinimalApiDiscovery;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,9 @@ builder.Services.AddDbContext<FootsContext>();
 
 var app = builder.Build();
 
-app.MapGet("/api/products", (FootsContext ctx) => ctx.Products.ToList());
+app.MapApis();
+
+//new ProductApi().Register(app);
+//app.MapGet("/api/products", (FootsContext ctx) => ctx.Products.ToList());
 
 app.Run();
